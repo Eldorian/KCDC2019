879e3c41d61f35eab95d979ee1cd1ecb7ef30efb
J. R. Cook <jr.cook@hallmark.com>
2019-07-17 14:08:21 -0500

    Adding sessions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+function App(){
+  const sessions =[
+    "React",
+    "C#",
+    "Python"
+  ];
+
+  //what I return here gets rendered
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <h1>KCDC Sessions</h1>{ sessions.map(session => <li>{session}</li>)}
+    </> 
+    );
+};
 
 export default App;
