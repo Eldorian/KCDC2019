5f2dc512a62adabb766b46675d446359b4e3db92
J. R. Cook <jr.cook@hallmark.com>
2019-07-17 14:10:42 -0500

    pretty code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
 
-function App(){
-  const sessions =[
-    "React",
-    "C#",
-    "Python"
-  ];
+function App() {
+  const sessions = ["React", "C#", "Python"];
 
   //what I return here gets rendered
   return (
     <>
-      <h1>KCDC Sessions</h1>{ sessions.map(session => <li>{session}</li>)}
-    </> 
-    );
-};
+      <h1>KCDC Sessions</h1>
+      <ul>
+        {sessions.map(session => (
+          <li>{session}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
 
 export default App;
